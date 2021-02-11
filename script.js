030edda50e48a6d7515d0ffd8cca7e305caa379a
Adlo777 <loadgoca@gmail.com>
2021-02-11 23:17:16 +0100

    Second

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let Personne = {
-    nom: ['Admeo', 'Golven'],
-    age: 21,
-    mail: 'loadgoca@gmail.com',
+function Ligne(rayon) {
+    this.rayon = rayon;
+}
+Ligne.prototype.taille = function() {
+    console.log(` Le Rayon est donné par la longuer suivante ${this.rayon}`);
+}
 
-    bonjour: function() {
-        console.log('Bienvenue ' + this.nom[0])
-    }
+function Cercle(rayon, phi) {
+    Ligne.call(this, rayon);
+    this.phi = phi;
+}
 
-};
+Cercle.prototype = Object.create(Ligne.prototype);
+Cercle.prototype.constructor = Cercle;
+Cercle.prototype.aire = function() {
+    console.log(`L'aire de ce cercle est donnée par ${Math.pow(this.rayon, 2)*Math.PI}`);
+}
 
-Personne.bonjour();
+function Sphere(rayon, phi, teta) {
+    Cercle.call(this, rayon, phi);
+    this.teta = teta;
+}
+
+Sphere.prototype = Object.create(Cercle.prototype);
+Sphere.prototype.constructor = Sphere;
+Sphere.prototype.volume = function() {
+    console.log(`Ceci est un volume imaginaire: ${Math.pow(this.rayon, 2)}*${Math.PI}*${this.phi}*${this.teta}`)
+}
+
+let geo = new Sphere(12, 20, 15);
+geo.taille();
+geo.aire();
+geo.volume();
